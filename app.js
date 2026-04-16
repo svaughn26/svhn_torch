f72870419cdd6c782dbbb4091132a00ee8c3df18
svaughn26 <shardaivaughn@gmail.com>
2026-04-16 18:52:11 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": []
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "V-kbzmTIkT64",
+        "outputId": "9ec8bb76-d034-474e-cf1e-4be9a24dade0"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Writing app.js\n"
+          ]
+        }
+      ],
+      "source": [
+        "%%writefile app.js\n",
+        "console.log(\"JS file loaded\");\n"
+      ]
+    }
+  ]
+}
